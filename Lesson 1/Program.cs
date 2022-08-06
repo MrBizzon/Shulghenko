47dcf7497cfa4856f2b66069bcb8461db1de035e
MrBizzon <oleg.zubarev@mail.ru>
2022-08-06 15:10:05 +0300

    dutya

--- Lesson 1/Program.cs
+++ Lesson 1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число!");
+int namber = int.Parse(Console.ReadLine());
+if (namber > 0)
+{
+    namber *= namber;
+    Console.WriteLine("Число, возведенное в квадрат: " + namber);
+}
+if (namber < 0)
+{
+    Console.WriteLine("Число отрицательное");
+}
+else
+{
+        Console.WriteLine("Веден ноль");
+}
