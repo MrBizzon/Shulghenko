f2560a42377eaac077bd35226fea25c34c666db4
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 18:02:12 +0300

    2

--- DZ lesson 05.08.2022/2/Program.cs
+++ DZ lesson 05.08.2022/2/Program.cs
@@ -2,11 +2,11 @@
 // три числа и выдаёт максимальное из этих чисел.
 
 Console.WriteLine("Enter the first number");
-int nuamberFirst = int.Parse(Console.ReadLine());
+int nuamberFirst = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number");
-int numberSecond = int.Parse(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the third number");
-int numberthird = int.Parse(Console.ReadLine());
+int numberthird = Convert.ToInt32(Console.ReadLine());
 int max = nuamberFirst;
 if (numberSecond > max)
     max = numberSecond;
