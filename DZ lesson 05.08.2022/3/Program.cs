96ac3fef79b6584e9b343103c805f6a21712cc9d
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 09:46:03 +0300

    Commit to the third task

---
+++ DZ lesson 05.08.2022/3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Homework number 3
+Console.WriteLine("Enter the number");
+int nuamber = int.Parse(Console.ReadLine());
+if (nuamber % 2 == 0)
+{
+    Console.WriteLine("The entered number is positive");
+}
+else
+{
+    Console.WriteLine("The entered number is negative");
+}
