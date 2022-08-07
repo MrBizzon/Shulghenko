94ddd2bd68eac2be1fa7a2f3bf1bd092aabe428d
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 13:20:32 +0300

    Commit to the third task

--- DZ lesson 05.08.2022/3/Program.cs
+++ DZ lesson 05.08.2022/3/Program.cs
@@ -1,4 +1,6 @@
-﻿// Homework number 3
+﻿// Homework number 3. Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число чётным
+
 Console.WriteLine("Enter the number");
 int number = int.Parse(Console.ReadLine());
 if (number % 2 == 0)
