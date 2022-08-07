ffbbb58d586d2c54c4e20da14eebb90440ac5db7
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 13:21:01 +0300

    Commit to the second task

--- DZ lesson 05.08.2022/2/Program.cs
+++ DZ lesson 05.08.2022/2/Program.cs
@@ -1,4 +1,6 @@
-﻿// Homework number 2
+﻿// Homework number 2. Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+
 Console.WriteLine("Enter the first number");
 int nuamberFirst = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter the second number");
