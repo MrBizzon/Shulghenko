3f8f0b0938dbf2c14d99fa4f1ae28986483cedfe
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 08:48:08 +0300

    Feed on the first taskэ=

---
+++ DZ lesson 05.08.2022/1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Homework number 1
+Console.WriteLine("Enter the first number");
+int nuamberFirst = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int numberSecond = int.Parse(Console.ReadLine());
+if (numberSecond > nuamberFirst)
+{
+    Console.WriteLine("A large number is " + numberSecond);
+}
+if (numberSecond < nuamberFirst)
+{
+    Console.WriteLine("A large number is " + nuamberFirst);
+}
+
+if (numberSecond == nuamberFirst)
+{
+    Console.WriteLine("The numbers are equal");
+}
