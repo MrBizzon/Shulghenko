2567ad1800b1a20c55371b848d4cd9ff6a70b4d7
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 09:24:30 +0300

    Commit to the second task

---
+++ DZ lesson 05.08.2022/2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Homework number 2
+Console.WriteLine("Enter the first number");
+int nuamberFirst = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int numberSecond = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third number");
+int numberthird = int.Parse(Console.ReadLine());
+int max = nuamberFirst;
+if (numberSecond > max)max = numberSecond;
+if (numberthird > max)max = numberthird;
+Console.WriteLine("Maximum number " + max);
