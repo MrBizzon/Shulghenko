7dc411dbf6e5d79fee96df8743752be1ef2f0ecc
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 18:06:50 +0300

    4

--- DZ lesson 05.08.2022/4/Program.cs
+++ DZ lesson 05.08.2022/4/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Homework number 4. Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N
 
-Console.WriteLine("Enter a positive number:");
+Console.WriteLine("Enter a positive number");
 int Number = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 Console.WriteLine("From 0 to " + Number + " even numbers are:");
