3cc272602528f705c27a261f6ce58a2919e1fd8f
MrBizzon <oleg.zubarev@mail.ru>
2022-08-07 11:11:38 +0300

    Commit to the fourth task

---
+++ DZ lesson 05.08.2022/4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Homework number 4
+Console.WriteLine("Enter a positive number:");
+int Number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine("From 0 to " + Number + " even numbers are:");
+while (i <= Number)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+
+if (Number <= 1)
+{
+    Console.Write("There are no positive numbers in the entered number");
+}
