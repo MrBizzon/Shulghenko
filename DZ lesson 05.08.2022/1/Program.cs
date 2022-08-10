282d73db818ad658adcbdc0cd584ae0a38448aa2
MrBizzon <oleg.zubarev@mail.ru>
2022-08-10 16:52:43 +0300

    1

--- DZ lesson 05.08.2022/1/Program.cs
+++ DZ lesson 05.08.2022/1/Program.cs
@@ -1,20 +1,20 @@
-﻿// Homework number 1. Напишите программу, которая на вход принимает два числа и выдаёт, 
+﻿// Homework number 1. Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
 
 Console.WriteLine("Enter the first number:");
-int nuamberFirst = Convert.ToInt32(Console.ReadLine());
+int numberFirst = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number:");
 int numberSecond = Convert.ToInt32(Console.ReadLine());
-if (numberSecond > nuamberFirst)
+if (numberSecond > numberFirst)
 {
-    Console.WriteLine("A large number is " + numberSecond);
+    Console.WriteLine($"A large number is {numberSecond}");
 }
-if (numberSecond < nuamberFirst)
+if (numberSecond < numberFirst)
 {
-    Console.WriteLine("A large number is " + nuamberFirst);
+    Console.WriteLine($"A large number is {numberFirst}");
 }
 
-if (numberSecond == nuamberFirst)
+if (numberSecond == numberFirst)
 {
     Console.WriteLine("The numbers are equal");
 }
