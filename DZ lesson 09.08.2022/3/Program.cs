fee05d979edbc63342628e0919043b9f3dee5f4f
MrBizzon <oleg.zubarev@mail.ru>
2022-08-10 16:00:45 +0300

    3

---
+++ DZ lesson 09.08.2022/3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7:");
+int number = Convert.ToInt32(Console.ReadLine());
+string[] week = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
+
+if (number < week.Length - 2) Console.Write($"{week[number - 1]} является рабочим днем");
+if (number >= week.Length - 2) Console.WriteLine($"{week[number - 1]} - это выходной");
