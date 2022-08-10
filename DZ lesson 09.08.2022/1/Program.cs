78e1eee58d485bb0d94d8e041e8e2594ff3a4c46
MrBizzon <oleg.zubarev@mail.ru>
2022-08-10 14:47:14 +0300

    1

---
+++ DZ lesson 09.08.2022/1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    string str = number.ToString();
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (i == 1)
+            Console.Write($"Второй цифрой числа {number} является {str[i]}");
+    }
+}
+else
+{
+    Console.WriteLine("Это число не является трехзначным");
+}
