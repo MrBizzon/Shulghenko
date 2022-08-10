3dee0f8f7429c03fa420d1e6fef27ababb556ea9
MrBizzon <oleg.zubarev@mail.ru>
2022-08-10 16:50:21 +0300

    3

--- DZ lesson 09.08.2022/3/Program.cs
+++ DZ lesson 09.08.2022/3/Program.cs
@@ -3,7 +3,10 @@
 
 Console.WriteLine("Введите число от 1 до 7:");
 int number = Convert.ToInt32(Console.ReadLine());
+
 string[] week = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
 
-if (number < week.Length - 2) Console.Write($"{week[number - 1]} является рабочим днем");
-if (number >= week.Length - 2) Console.WriteLine($"{week[number - 1]} - это выходной");
+if (number <= week.Length - 2) Console.WriteLine($"{week[number - 1]} является рабочим днем");
+if (number > week.Length -2 && number <= week.Length) Console.WriteLine($"{week[number - 1]} - это выходной");
+if (number > week.Length) Console.WriteLine("Не надо хулиганить");
+
