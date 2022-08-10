902ebfd8645987a8d2b79781583bf12c65b04911
MrBizzon <oleg.zubarev@mail.ru>
2022-08-10 15:08:36 +0300

    2

--- DZ lesson 09.08.2022/2/Program.cs
+++ DZ lesson 09.08.2022/2/Program.cs
@@ -1,5 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
 
+Console.WriteLine("Введите любое число:");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine($"Искомое число = {number / 100}{number % 10}");
+if (number < 100 && number > 0)
+    Console.WriteLine("Введенное число двузначное, третья цифра отсутствует");
+if (number > 99)
+{
+    string str = number.ToString();
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (i == 2)
+            Console.Write($"Третьей цифрой числа {number} является {str[i]}");
+    }
+}
