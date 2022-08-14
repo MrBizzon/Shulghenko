48271641355b37e2b1a25ba185ab31d3e59a5775
MrBizzon <oleg.zubarev@mail.ru>
2022-08-14 09:19:23 +0300

    commit 1

---
+++ Lesson 3/1/Program.cs
@@ -0,0 +1,20 @@
+﻿void OOO(string text)
+{
+if (string.IsNullOrEmpty(text))
+{
+    Console.WriteLine("Пустая строка");
+    return; 
+}
+Console.WriteLine(text);
+}
+
+
+int Sum(int x, int y)
+{
+    return x + y;
+}
+
+OOO("");
+OOO("Теперь не пустая строка");
+int result = Sum(5, 8);
+OOO(result.ToString());
